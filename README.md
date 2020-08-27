f7d77a214d8652accbd1ca52c82a2af3ba0cdda6
Steven <steventdev@gmail.com>
2020-08-27 01:40:16 -0400

    added readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# prerequisites
+
+- Have Chrome installed
+- Have Node Installed
+
+# running
+
+- cd to the src folder in terminal
+- Run command node bot.js
+- In Discord use: !scrape (USERNAME)
--- src/bot.js
+++ src/bot.js
@@ -4,34 +4,36 @@ const config = require("./config.json");
 const client = new Discord.Client();
 
 const scrapeProfile = async (username) => {
-    let x = new Promise(async(resolve, reject) => {
-          const browser = await puppeteer.launch({headless: false});
-          const page = await browser.newPage();
-          await page.goto(`https://www.instagram.com/${username}/`);
-          const imgList = await page.evaluate(()=>{
-              const nodeList = document.querySelectorAll("article img");
+  let x = new Promise(async (resolve, reject) => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto(`https://www.instagram.com/${username}/`);
+    const imgList = await page.evaluate(() => {
+      const nodeList = document.querySelectorAll("article img");
 
-              if(nodeList.length === 0) {
-                  return {message: "Sorry, this Account is Private, isn't available or may have been removed."}
-              }
-              const imgArray = [...nodeList];
-              const imgList = imgArray.map(({ src }) => ({src,}));
-              return imgList;
-          })
-          await browser.close();
-          resolve(imgList);
-        });
-return await x;
+      if (nodeList.length === 0) {
+        return {
+          message:
+            "Sorry, this Account is Private, isn't available or may have been removed.",
         };
+      }
+      const imgArray = [...nodeList];
+      const imgList = imgArray.map(({ src }) => ({ src }));
+      return imgList;
+    });
+    await browser.close();
+    resolve(imgList);
+  });
+  return await x;
+};
 
-client.on('ready', () => {
-  console.log('Bot Online')
-  client.user.setActivity('!scrape (username)', { type: 'WATCHING' })
+client.on("ready", () => {
+  console.log("Bot Online");
+  client.user.setActivity("!scrape (username)", { type: "WATCHING" });
 });
 
-client.on('message', async (message) => {
+client.on("message", async (message) => {
   if (message.content.startsWith(`${config.prefix}scrape`)) {
-
     let username = message.content.split(" ");
     if (username[0] === "!scrape") {
       message.channel.send(`**Scraper Launched**`);
@@ -41,13 +43,11 @@ client.on('message', async (message) => {
         //const imgList0 = imgList.slice(0, 1);
         // imgList0.map((x) => console.log(x.src))
         imgList.map((x) => {
-            message.channel.send(x.src);
+          message.channel.send(x.src);
         });
-
+      }
     }
-    }
-    }
-  });
-
+  }
+});
 
 client.login(config.token);
